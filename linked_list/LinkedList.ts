a7ce0127726ccc895f04278a229c909d09fb0d7d
brendadev <brenda@koneksahealth.com>
2022-03-23 22:20:50 -0400

    starter code

---
+++ linked_list/LinkedList.ts
@@ -0,0 +1,20 @@
+
+function createLinkedList () {
+  // const myLinkedList = new LinkedList(10);
+  // @ts-ignore
+  // console.log({myLinkedList});
+  
+  // @ts-ignore
+  // console.log(myLinkedList.append(5));
+  // @ts-ignore
+  // console.log({myLinkedList});
+  // @ts-ignore
+  // console.log(myLinkedList.head.next);
+
+  // @ts-ignore
+  // console.log(myLinkedList.append(9));
+  // @ts-ignore
+  // console.log({myLinkedList});
+}
+
+createLinkedList();
